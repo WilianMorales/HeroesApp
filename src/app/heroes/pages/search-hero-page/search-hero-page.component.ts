621fa3536a8000aba89e5c6cf13e1e96ab6fe8f5
Wilian Morales <willian.moralesch@gmail.com>
2024-07-24 12:56:15 -0500

    Implementation of the components and structure of the project.

---
+++ src/app/heroes/pages/search-hero-page/search-hero-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-hero-page',
+  templateUrl: './search-hero-page.component.html',
+  styles: [
+  ]
+})
+export class SearchHeroPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
