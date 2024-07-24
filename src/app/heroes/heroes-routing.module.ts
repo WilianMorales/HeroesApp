75814a8382e54bb7811edfbe9823bdb7a48f768c
Wilian Morales <willian.moralesch@gmail.com>
2024-07-24 13:33:02 -0500

    Implementation of routes and correction of the error in tsconfig.

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewHeroPageComponent } from './pages/new-hero-page/new-hero-page.component';
+import { SearchHeroPageComponent } from './pages/search-hero-page/search-hero-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      {
+        path: 'new-heroe',
+        component: NewHeroPageComponent
+      },
+      {
+        path: 'search',
+        component: SearchHeroPageComponent
+      },
+      {
+        path: 'edit/:id',
+        component: NewHeroPageComponent
+      },
+      {
+        path: 'list-heroes',
+        component: ListPageComponent
+      },
+      {
+        path: 'by/:id',
+        component: HeroPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'list-heroes'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
