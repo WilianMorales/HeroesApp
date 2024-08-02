8166ac6d36e306622db1df1524b4750de76bba7b
Wilian Morales <willian.moralesch@gmail.com>
2024-08-02 15:44:59 -0500

    UPDATE of assets.

--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -5,19 +5,7 @@ import { HeroesService } from '@heroes/services/heroes.service';
 @Component({
   selector: 'app-list-page',
   templateUrl: './list-page.component.html',
-  styles: [`
-    .grid-card{
-      display: grip;
-      gap: 1rem;
-      grip-auto-flow: dense;
-      grip-auto-rows: 22rem;
-      grip-template-columns: repeat(auto-fill, minmax(15rem, 1fr));
-    }
-
-    .featured {
-      grid-column: span 2;
-    }
-    `
+  styles: [
   ]
 })
 export class ListPageComponent implements OnInit {
