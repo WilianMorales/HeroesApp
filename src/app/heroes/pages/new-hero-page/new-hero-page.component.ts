1c76b6adde8e649c724bb0c3563f9d55d752b127
Wilian Morales <willian.moralesch@gmail.com>
2025-01-21 16:15:59 -0500

    Implementation of the reactive form in the component, as well as the methods to register and update a hero.

--- src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
+++ src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
@@ -1,21 +1,27 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Hero, Publisher } from '@heroes/interfaces/hero.interface';
+import { HeroesService } from '@heroes/services/heroes.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-new-hero-page',
   templateUrl: './new-hero-page.component.html',
-  styles: [
+  styles: [`
+  .responsive-card { width: 100%; max-width: 400px; margin: 0 auto 0 0; }
+  .responsive-img { height: 500px; } `
   ]
 })
-export class NewHeroPageComponent {
+export class NewHeroPageComponent implements OnInit {
 
   public heroForm = new FormGroup ({
-    id: new FormControl(''),
-    superhero: new FormControl('', Validators.required),
-    publisher: new FormControl(''),
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>( Publisher.DCComics ),
     alter_ego: new FormControl(''),
     first_appearance: new FormControl(''),
-    character: new FormControl(''),
+    characters: new FormControl(''),
     alt_img: new FormControl(''),
   });
 
@@ -24,4 +30,46 @@ export class NewHeroPageComponent {
     { id: 'Marvel Comics', desc: 'Marvel - Comics' }
   ]
 
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
+  ngOnInit(): void {
+    if ( !this.router.url.includes('edit')) return;
+
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({ id }) => this.heroesService.getHeroById(id) ),
+      ).subscribe( hero => {
+        if ( !hero ) return this.router.navigateByUrl('/');
+
+        this.heroForm.reset( hero );
+        return;
+      });
+  }
+
+  onSubmit(): void {
+    if ( this.heroForm.invalid ) return;
+
+    if ( this.currentHero.id ) {
+      this.heroesService.updateHero( this.currentHero )
+        .subscribe( hero => {
+          // TODO: mostrar snackbar
+        });
+      return;
+    }
+
+    this.heroesService.addHero( this.currentHero )
+      .subscribe( hero => {
+        // TODO: mostrar snackbar, y navegar a /heroes/edit/ hero.id
+      });
+
+  }
 }
